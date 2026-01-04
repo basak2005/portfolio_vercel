e05f2a398d75bee92a0c6776a255fbd344e74d27
basak2005 <souranilbasak@gmail.com>
2026-01-04 10:15:27 +0530

    Remove Render configuration files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn --pythonpath portfolio portfolio.wsgi
--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build script for Vercel deployment
-pip install -r requirements.txt
-
-# Collect static files
-cd portfolio
-python ../manage.py collectstatic --noinput
